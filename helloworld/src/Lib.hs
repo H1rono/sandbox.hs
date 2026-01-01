4b09d4fa9717b257b6221854e23e23cbdf8aaa8a
H1rono <hronok66@gmail.com>
2026-01-01 16:54:01 +0900

    :sparkles: Add `threeCoins`

--- helloworld/src/Lib.hs
+++ helloworld/src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib (
     helloWorld,
+    threeCoins,
 ) where
+
+import System.Random (RandomGen, random)
 
 helloWorld :: IO ()
 helloWorld = putStrLn "Hello, world!"
+
+-- >>> import System.Random (mkStdGen)
+-- >>> threeCoins (mkStdGen 42)
+-- (False,True,True)
+threeCoins :: (RandomGen g) => g -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (first, newGen) = random gen
+        (second, newGen') = random newGen
+        (third, _newGen'') = random newGen'
+     in (first, second, third)
