600b0a2cdd58b3ee3a5ed79523ca64542a619737
H1rono <hronok66@gmail.com>
2026-01-01 16:45:52 +0900

    :sparkles: Hello, world!

--- helloworld/src/Lib.hs
+++ helloworld/src/Lib.hs
@@ -1,6 +1,6 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    helloWorld,
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello, world!"
